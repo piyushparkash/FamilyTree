9f8b89f9ea00a2356ae71529232ca049afe5bb5f
Piyush <achyutapiyush@gmail.com>
2012-09-22 17:21:00 +0530

    Made basic changes due related to manual header inclusion

--- install/install.php
+++ install/install.php
@@ -5,10 +5,6 @@
  * @package install
  * @author piyush
  */
-
-
-
-
 $mode = @$_GET['mode'];
 $sub = @$_GET['sub'];
 
@@ -18,7 +14,7 @@ class install {
      */
 
     function install() {
-        
+        global $mode, $sub;
         //make sure to run this only if database is not installed
         //So check if database is installed
         if (!empty($config) and file_exists("../config.php")) {
@@ -45,15 +41,24 @@ class install {
      * @param string $sub Describes which part is running of the phase
      */
     function ask_database_name($mode, $sub) {
-        global $template, $config, $db;
+        global $template, $db;
         $sub = ($sub == null) ? 1 : $sub;
         if ($sub == 1) {
+            $template->header();
             $template->display("install.ask_database_details.tpl");
         } elseif ($sub == 2) {
             $host = $_POST['database_host'];
             $username = $_POST['database_username'];
             $password = $_POST['database_password'];
             $database = $_POST['database_name'];
+
+            if (empty($host) || empty($username) || empty($password) || empty($database)) {
+                $template->header();
+                $template->assign(array("error" => 1,
+                    "message" => "Form not completed. Please complete the form"));
+                $template->display("install.ask_database_details.tpl");
+                return;
+            }
 
             //Connect to database
             $db->connect($host, $username, $password);
@@ -86,8 +91,17 @@ class install {
      * Function to setup the database
      */
     private function setup_database() {
+        global $db;
         //Read the basic table schema and execute it
+        $scheme_file = fopen("schema.sql", "r");
+        $data = fread($scheme_file, filesize("schema.sql"));
+        $db->query($data);
+        fclose($scheme_file);
         //now enter the data that we have
+        $member_data = fopen("member_data.sql", "r");
+        $data = fread($member_data, filesize("member_data.sql"));
+        $db->query($data);
+        fclose($member_data);
     }
 
 }
