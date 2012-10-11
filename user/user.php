900bcb79ba0b42a51c91ad4be467572452a3c9e3
Piyush <achyutapiyush@gmail.com>
2012-10-11 21:39:19 +0530

    Added populate_data functions

--- user/user.php
+++ user/user.php
@@ -11,27 +11,32 @@
  * @author piyush
  */
 
-require("../auth/auth.php");
+require("auth/auth.php");
 
 class user extends auth {
     public $user=array();
     public function __construct() {
         parent::__construct();
+        global $_SESSION;
         if ($this->check_session()==true)
         {
             //populate user data and set class variables to authenticated else normal
+            $this->populate_data($_SESSION['id']);
         }
     }
     
     
     function login($username,$password)
     {
-        return authenticate($username,$password);
+        return $this->authenticate($username,$password);
     }
     
-    function populate_data()
+    function populate_data($id)
     {
-        
+        global $db;
+        $query=$db->query("Select * from member where id=$id");
+        $row=$db->fetch($query);
+        $this->user=$row;
     }
 }
 
