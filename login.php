f58ab19877faa13318262a17847ca6cba3e9f9aa
Piyush <achyutapiyush@gmail.com>
2012-10-11 21:40:06 +0530

    Now using classes

--- login.php
+++ login.php
@@ -1,36 +1,33 @@
 <?php
 
 /**
- * @author lolkittens
+ * @author Piyush
  * @copyright 2012
  */
 require "header.php";
-if (isset($_POST['username'],$_POST['password']))
-{
-$username = $_POST['username'];
-$pass = $_POST['password'];
-$finalarray = array();
-connecttodatabase();
-$query = executequery("select * from member where username='$username' and password='$pass'");
-$row = mysql_fetch_array($query);
-if (!$row) {
-    $finalarray['error'] = 1;
-    echo json_encode($finalarray);
-    exit();
+global $user;
+
+if (isset($_POST['username'], $_POST['password'])) {
+    $username = $_POST['username'];
+    $pass = $_POST['password'];
+    
+    $ret = $user->login($username, $pass);
+    if ($ret['error'] == 2) { //If user hasn't activated his account
+        echo json_encode(array("error" => 2)); //The error code is 2
+        exit();
+    }
+        if (!$user->is_authenticated()) {
+            $finalarray['error'] = 1;
+            echo json_encode($finalarray);
+            exit();
+        } else {
+            
+        }
+
+        $finalarray = array('membername' => $user->user['membername'], 'id' => $user->user['id']);
+        echo json_encode($finalarray);
+    
 } else {
-	if ($row['approved']==0) //If user hasn't activated his account
-	{
-		echo json_encode(array("error" => 2)); //The error code is 2
-		exit();
-	}
-	//Else
-    authenticateuser($row['id'], $row['membername']);
-    $finalarray = array('membername' => $row['membername'], 'id' => $row['id']);
-    echo json_encode($finalarray);
-}
-}
-else
-{
-    header("Location:index.html");
+    header("Location:index.php");
 }
 ?>
