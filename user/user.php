c84a367423a44c1c0f7298dbb87ce08b0e05f70e
Piyush <achyutapiyush@gmail.com>
2012-10-12 20:09:33 +0530

    Added remove_data, populate data after login, Added Logout Function

--- user/user.php
+++ user/user.php
@@ -10,34 +10,56 @@
  *
  * @author piyush
  */
-
 require("auth/auth.php");
 
 class user extends auth {
-    public $user=array();
+
+    public $user = array();
+
     public function __construct() {
         parent::__construct();
         global $_SESSION;
-        if ($this->check_session()==true)
-        {
+        if ($this->check_session() == true) {
             //populate user data and set class variables to authenticated else normal
             $this->populate_data($_SESSION['id']);
         }
     }
-    
-    
-    function login($username,$password)
-    {
-        return $this->authenticate($username,$password);
+
+    function login($username, $password) {
+        global $_SESSION;
+
+        //Login
+        $ret = $this->authenticate($username, $password);
+
+        //Populate the user array only if user has logged in
+        if ($ret == true and !is_array($ret)) {
+            $this->populate_data($_SESSION['id']);
+        }
+        
+        //And return as it was previously doing before populate data was added
+        return $ret;
     }
-    
-    function populate_data($id)
-    {
+
+    function logout() {
+        //Remove all the session variables and remove user data
+        $this->unauthenticate();
+        $this->remove_data();
+    }
+
+    function populate_data($id) {
+        // Fill user variable with user data
         global $db;
-        $query=$db->query("Select * from member where id=$id");
-        $row=$db->fetch($query);
-        $this->user=$row;
+        $query = $db->query("Select * from member where id=$id");
+        $row = $db->fetch($query);
+        $this->user = $row;
     }
+
+    function remove_data() {
+        //Remove data filled in user variable
+        unset($this->user);
+        $this->user = array();
+    }
+
 }
 
 ?>
