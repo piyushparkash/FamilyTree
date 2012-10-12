e8941c3a8e5be861b9c128f20c46ed53c93e4268
Piyush <achyutapiyush@gmail.com>
2012-10-13 01:19:19 +0530

    Individual Page for register

---
+++ register.php
@@ -0,0 +1,33 @@
+<?php
+
+require 'header.php';
+global $template, $vanshavali, $user;
+
+//If there is submitted data
+if (isset($_POST['register_submit'])) {
+    //Collect all the variables
+    $id = $_POST['register_id'];
+
+    $username = $_POST['register_username'];
+
+    $password = $_POST['register_password'];
+
+    //Convert date to Unix TimeStamp
+    preg_match("/([0-9]{1,2})\/([0-9]{1,2})\/([0-9]{4,4})/", $_POST['register_dob'], $matches);
+    $dob = mktime(0, 0, 0, $matches[2], $matches[1], $matches[3]);
+
+    $gender = $_POST['register_gender'];
+    $relation = $_POST['register_relationship'];
+    $gaon = $_POST['register_gaon'];
+    $email = $_POST['register_email'];
+    $about = $_POST['register_about'];
+
+    $vanshavali->register(array(
+        $username,$password,$dob,$gender,$relation,$gaon,$email,$about,$id
+    ));
+}
+
+$template->header();
+$template->display('user.main.tpl');
+$template->display('register.form.tpl');
+$template->footer();
