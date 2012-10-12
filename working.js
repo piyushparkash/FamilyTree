560fe7f003cc971bb3abbbd0d080b33226202dfe
Piyush <achyutapiyush@gmail.com>
2012-10-13 01:20:20 +0530

    Moved Register, Fixed Bugs

--- working.js
+++ working.js
@@ -9,168 +9,8 @@ $(document).ready(function () {
         "width":opti_width,
         "height":opti_height
     });
-//	register();
 }
 );
-//Register Related Functions
-function register()
-{  
-    //Show the register dialog
-    $("#register").modal();
-    
-    
-    //Function to display text with the input controls in the register form
-    function success_display(id,display_text)
-    {
-        //hide the element, insert the text and the tick, and then show
-        $("#"+id).parents(".control-group").addClass("success");
-        $("#"+id).siblings(".help-text").css("display","none").innerHTML(display_text)
-        .fadeIn(500);
-    }
-    
-    //Function to hide success message when value changed
-    function success_hide(id)
-    {
-        $("#"+id).siblings("span.help-text").fadeIn("medium");
-        $("#"+id).parents("div.control-group").removeClass("success");
-    }
-    
-    
-    //Initialize constraints and checks
-    //Name related checks
-    $("#register_name").autocomplete({
-        source: function(request, response) {
-            var newarray = new Array() //This array will hold all the results
-            
-            
-            $.getJSON("register_username.php?action=register&pt=" + request.term, "", function(data) {
-                $.each(data, function(key, value) {
-                    //Convert to jquery-ui obect
-                    var obj = {
-                        label: value,
-                        value: key
-                    };
-                    //push it in the final array
-                    newarray.push(obj);
-                    
-                });
-                
-                response(newarray);
-            });
-        },
-        select: function(e, ui) {
-            //Set the value to the key instead of value
-            this.value = ui.item.label;
-            
-            //save the selected member id in the hidden input
-            $("#register_id").val(ui.item.value);
-			
-            //show the success text
-            success_display("register_name","Correct Name")
-            return false;
-            
-        },
-        focus: function() {
-            return false;
-        }
-    });
-	
-    //end of name related checks
-	
-    //Username related checks
-    $("#register_username").blur(function () //when user leaves the control
-    {
-        $.post("getdata.php",{
-            action:"username_check",
-            username:this.value
-        },function (data)
-
-        {
-                var json=$.parseJSON(data);
-                if (json.yes) //if reply is yes the username is already used 
-                {
-                    alert("Username is already used. Try something else");
-                    $(this).focus();
-                    //hide any previous success message
-                    success_hide("register_username");
-                }
-                else //else not used and can be used
-                {
-                    success_display("register_username","Valid Username");
-                }
-            });
-    });
-    //end of username related checks
-	
-	
-    //Password related checks
-    $("#register_confirmpassword").blur(function ()
-    {
-        var password=$("#register_password");
-        if (this.value==password.val())
-        {
-            success_display("register_confirmpassword","Password Matched");
-        }
-        else
-        {
-            alert("Password donot match. Please check again.");
-            password.focus();
-			
-            //hide any previous success message
-            success_hide("register_confirmpassword");
-        }
-    });
-    //end of password related checks
-	
-    //Date of Birth related checks
-    //Initialize the date picker
-    $("#register_dob").datepicker({
-        maxDate:"-1d",
-        dateFormat:"dd/mm/yy"	
-    });
-	
-    //End of Date of Birth Related Checks
-	
-	
-    //Email related checks
-    $("#register_email").blur(function () {
-        x=this.value;
-        var atpos=x.indexOf("@");
-        var dotpos=x.lastIndexOf(".");
-        if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length)
-        {
-            success_hide("register_email");
-            alert("Not a valid e-mail address");
-            $(this).focus();
-        }
-        else
-        {
-            //display success text
-            success_display("register_email","Valid Email Id");
-        }
-    });
-    //end of email related check
-	
-    //All checks performed now we can submit the form
-    $("#register_form").submit(function () {
-		
-        //will remain true if everything is filled out
-        var passed=true;
-        var check=$("#register_name,#register_username,#register_password,#register_confirmpassword,#register_dob,#register_email");
-        $.each(check,function (key,value) {
-            if (value.value=='') //if value of specified element is empty
-            {
-                value.focus(); //focus on that variable
-                passed=false;  //set passed to false;
-                return false;
-            }
-        });
-        return passed;
-    });
-	
-}
-
-//end of register relateed Functions
 
 //Search Functions
 function search() {
@@ -254,12 +94,15 @@ function login_submit() {
         }
         if (parseInt(data.error) == 2)
         {
-            $("#login_error").html("Please activate your account by with the link sent to you email");
-            $("#login_error").html("Login Failed! Please check your username and password and try again!");
+            $("#login_error").html("Please activate your account by with the link sent to you on you email");
+            //$("#login_error").html("Login Failed! Please check your username and password and try again!");
             $("#login_username").removeAttr("disabled").val("");
             $("#login_password").removeAttr("disabled").val("");
+            
+            $("#login_error").removeClass("hide");
+            
             //positon the dialog in the center as #login_error is now visible
-            $("#login").dialog();
+            $("#login").modal();
             return false;
         }
             
