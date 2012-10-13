004c4919c5da229e098443b30df4aa974122940a
Piyush <achyutapiyush@gmail.com>
2012-10-13 12:16:24 +0530

    Member Class

---
+++ vanshavali/member.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * Class that manipulates Members
+ *
+ * @author piyush
+ */
+
+require_once 'suggest.php';
+class member extends suggest {
+
+    public $id, $data;
+
+    public function __construct($memberid) {
+        parent::__construct($memberid);
+        $id = $memberid;
+        $this->populate_data($memberid);
+    }
+    
+    function getparent()
+    {
+        return new member($this->data['sonof']);
+    }
+
+    function get_sons() {
+        global $db;
+        $finalarray=array();
+        $query=$db->query("select * from member where sonof=$this->id");
+        while ($row=$db->fetch($query))
+        {
+            array_push($finalarray, new member($row['id']));
+        }
+        return $finalarray;
+    }
+
+    function has_sons() {
+        global $db;
+        $query=$db->query("select count(*) as nosons from member where sonof=$this->id");
+        $row=$db->fetch($query);
+        return $row['nosons'];
+    }
+
+    function populate_data($memberid) {
+        // Fill user variable with user data
+        global $db;
+        $query = $db->query("Select * from member where id=$memberid");
+        $row = $db->fetch($query);
+        $this->data = $row;
+    }
+
+}
+
+?>
