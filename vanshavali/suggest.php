89375ed4e114df59a0eef1c920f644f79f45f131
Piyush <achyutapiyush@gmail.com>
2012-10-19 01:44:19 +0530

    Added edit function

--- vanshavali/suggest.php
+++ vanshavali/suggest.php
@@ -11,16 +11,18 @@
  * @author piyush
  */
 class suggest {
+
     public $id;
+
     public function __construct($memberid) {
         $this->id = $memberid;
     }
 
-    function add_son($name,$gender,$sonof) {
-        global $db,$user;
+    function add_son($name, $gender, $sonof) {
+        global $db, $user;
 
         //fill array with data
-        $finalarray=array('name'=>$name, 'gender'=>$gender ,'sonof'=>$sonof);
+        $finalarray = array('name' => $name, 'gender' => $gender, 'sonof' => $sonof);
 
         //put in the database
         //echo "insert into suggested_info (typesuggest,suggested_value,suggested_by,ts) values('child', '" .
@@ -31,14 +33,26 @@ class suggest {
     }
 
     function remove() {
-        global $db,$user;
-        
-        $query=$db->query("insert into suggested_info (typesuggest,suggested_value,suggested_by,ts) values
-            ('remove', '$this->id',". $user->user['id'].",".time().")");
+        global $db, $user;
+
+        $query = $db->query("insert into suggested_info (typesuggest,suggested_value,suggested_by,ts) values
+            ('remove', '$this->id'," . $user->user['id'] . "," . time() . ")");
     }
 
-    function edit($type) {
-        global $db,$user;
+    function edit($name, $gender, $relationship, $dob, $alive) {
+        global $db, $user;
+
+        preg_match("/([0-9]{1,2})\/([0-9]{1,2})\/([0-9]{4,4})/", $dob, $matches);
+        $dob = mktime(0, 0, 0, $matches[2], $matches[1], $matches[3]);
+
+        $finalarray = array('name' => $name,
+            'gender' => $gender,
+            'relationship' => $relationship,
+            'dob' => $dob,
+            'alive' => $alive);
+
+        $db->query("insert into suggested_info (typesuggest,suggested_value,suggested_by,ts) values
+            ('edit', '" . json_encode($finalarray) . "'," . $user->user['id'] . "," . time() . ")");
     }
 
 }
