d1eb716eb300ea4fe16c7cd13f208937275ead9d
piyush <piyush@piyush-Inspiron-N5110>
2012-12-27 22:02:33 +0530

    Now contains permanent addition or removal function

---
+++ vanshavali/member_operation.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * Contains functions to permanently add, remove or edit Member
+ * To be Only used by other classes
+ *
+ * @author piyush
+ */
+require 'member_operation_suggest.php';
+
+class member_operation extends member_operation_suggest {
+    
+    public $id;
+    public function __construct($memberid) {
+        $this->id=$memberid;
+    }
+    function add_son($name, $gender, $suggest) {
+        if ($suggest) {
+            parent::add_son_suggest($name, $gender, $this->data['id']);
+        } else {
+            //Add son directly to the Member database
+            global $db;
+
+            //Prepare the sql
+            $sql = "Insert into member(membername,gender,sonof) values('$name',$gender," . $this->data['id'] . ")";
+
+            //Execute the sql
+            if (!$db->get($sql)) {
+                trigger_error("Cannot add member. Error executing the query",E_USER_ERROR);
+            }
+        }
+    }
+
+    function remove($suggest) {
+        if ($suggest) {
+            parent::remove_suggest($this->data['id']);
+        } else {
+            //Remove the member completely
+            global $db;
+
+            //Prepare the sql
+            if (!$db->get("Update member set dontshow=1 where id=" . $this->data['id'])) {
+                trigger_error("Cannot delete member. Error Executing the query",E_USER_ERROR);
+            }
+        }
+    }
+
+    function edit($name, $gender, $relationship, $dob, $alive, $suggest) {
+        if ($suggest) {
+            parent::edit_suggest($name, $gender, $relationship, $dob, $alive, $this->data['id']);
+        } else {
+            //Change the details directly...
+            global $db;
+            
+            //Prepare the sql and execute it...
+            if (!$db->get("Update member set membername='$name',gender=$gender,
+                relationship_status=$relationship,dob=$dob, alive=$alive where id=".$this->data['id']))
+            {
+                trigger_error("Error Editing member. Error Executing query", E_USER_ERROR);
+            }
+        }
+    }
+}
+
+?>
