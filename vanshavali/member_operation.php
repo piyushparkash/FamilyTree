c59f973c5ed851ecdc913339447bb2b5baabe15d
piyush <piyush@piyush-Inspiron-N5110>
2012-12-28 10:28:52 +0530

    member operations now trigger notice level error Soo Execution of other things won't stop if error occurs

--- vanshavali/member_operation.php
+++ vanshavali/member_operation.php
@@ -26,7 +26,8 @@ class member_operation extends member_operation_suggest {
 
             //Execute the sql
             if (!$db->get($sql)) {
-                trigger_error("Cannot add member. Error executing the query",E_USER_ERROR);
+                trigger_error("Cannot add member. Error executing the query");
+                return false;
             }
         }
     }
@@ -40,7 +41,8 @@ class member_operation extends member_operation_suggest {
 
             //Prepare the sql
             if (!$db->get("Update member set dontshow=1 where id=" . $this->data['id'])) {
-                trigger_error("Cannot delete member. Error Executing the query",E_USER_ERROR);
+                trigger_error("Cannot delete member. Error Executing the query");
+                return false;
             }
         }
     }
@@ -56,7 +58,8 @@ class member_operation extends member_operation_suggest {
             if (!$db->get("Update member set membername='$name',gender=$gender,
                 relationship_status=$relationship,dob=$dob, alive=$alive where id=".$this->data['id']))
             {
-                trigger_error("Error Editing member. Error Executing query", E_USER_ERROR);
+                trigger_error("Error Editing member. Error Executing query");
+                return FALSE;
             }
         }
     }
