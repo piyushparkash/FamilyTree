35be3d40f21344e531d02b5ba051119d30a3e207
piyush <piyush@piyush-Inspiron-N5110>
2013-01-03 15:08:18 +0530

    Now return json error if request is ajax Removed useless functions Added Ajax interface function

--- header.php
+++ header.php
@@ -22,14 +22,14 @@ require_once 'user/user.php';
 require_once 'vanshavali/vanshavali.php';
 $template = new template();
 $db = new db();
-$vanshavali=new vanshavali();
+$vanshavali = new vanshavali();
 
 //Select the defualt database
 if (isset($config['database']) and !empty($config['database'])) {
     $db->select_db($config['database']);
 }
 
-$user=new user();
+$user = new user();
 
 //Initialize custom error handler
 function vanshavali_error($level, $message, $file, $line, $context) {
@@ -46,52 +46,24 @@ function vanshavali_error($level, $message, $file, $line, $context) {
             exit(); //exit the file as error level is high
             break;
         case E_USER_NOTICE:
-            $template->assign("message", $message);
-            $template->assign("lineno", $line);
-            $template->assign("file", $file);
-            $template->assign("context", $context);
-            $template->display("error_low.tpl");
+            //If request is AJAX then
+            if ($_SERVER['HTTP_X_REQUESTED_WITH']) {
+                //Prepare the array
+                $errorarray = array("success" => 0, "message" => $message);
+                echo json_encode($errorarray);
+            } else {
+                $template->assign("message", $message);
+                $template->assign("lineno", $line);
+                $template->assign("file", $file);
+                $template->assign("context", $context);
+                $template->display("error_low.tpl");
+            }
     }
 }
 
 set_error_handler("vanshavali_error");  //Set the custom error handler
 
 
-function hassons($memberid) {
-    $query = executequery("select * from member where sonof=" . $memberid);
-    if (mysql_fetch_row($query) == false) {
-        return false;
-    } else {
-        return true;
-    }
-}
-
-function deletesons($memberid) {
-    $query = executequery("delete from member where sonof=" . $memberid);
-    return $query;
-}
-
-function fatherid($memberid) {
-    $query = executequery("select* from member where id=" . $memberid);
-    return mysql_result($query, 0, "sonof");
-}
-
-function navigationstring($memberid) {
-    $current = $memberid;
-    $temp = $current;
-    $father = "";
-    $resultantstring = "";
-    while (1) {
-        $father = mysql_fetch_array(executequery("select * from member where ID=" . $temp));
-        $resultantstring = "<a href='index.php?showmember=" . $father['ID'] . "'>" . $father['membername'] .
-                "</a>>" . $resultantstring;
-        $temp = $father['sonof'];
-        if ($temp == null) {
-            break;
-        }
-    }
-    echo $resultantstring;
-}
 
 function fileext($filename, $ext = true) {
     $filename = basename($filename);
@@ -102,4 +74,14 @@ function fileext($filename, $ext = true) {
     } else {
         return implode("", array_slice($arr, 0, $count - 1));
     }
+}
+
+function ajaxSuccess()
+{
+    echo json_encode(array("success" => 1));
+}
+
+function ajaxError()
+{
+    echo json_encode(array("success" => 0));
 }
