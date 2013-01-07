2da5e5d3dce028054adfc2c9194b4498c60ae03c
piyush <piyush@piyush-Inspiron-N5110>
2013-01-08 00:08:40 +0530

    Was not reading from sql file so manually made file for database structure. every change should be made here

--- install/install.php
+++ install/install.php
@@ -74,30 +74,27 @@ class install {
 
             //Now create the config.php file save it
             $file = fopen("config.php", "w+");
-            if (!$file)
-            {
-                trigger_error("Error opening or creating config.php file",E_USER_ERROR);
+            if (!$file) {
+                trigger_error("Error opening or creating config.php file", E_USER_ERROR);
             }
-            
+
             $data = "<?php\n\$config['host']='$host';
                     \n\$config['username']='$username';
                     \n\$config['password']='$password';
                     \n\$config['database']='$database';
                     \n?>";
 
-            $wr=fwrite($file, $data);
+            $wr = fwrite($file, $data);
             fclose($file);
-            
+
             //Set file permission to 0644, Never leave this 0
-            if (!chmod("config.php", 0644))
-            {
+            if (!chmod("config.php", 0644)) {
                 //Read and write for the owner and read for everyone else
-                trigger_error("Cannot set config.php permissions",E_USER_ERROR);
-                
+                trigger_error("Cannot set config.php permissions", E_USER_ERROR);
+
                 //Check if permissions have been successfull applied or not
-                if (!is_readable("config.php"))
-                {
-                trigger_error("Wrong config.php permissions. Please give config.php file 644 permission. <br> Use the Following command<br>$ chmod 644 config.php",E_USER_ERROR);
+                if (!is_readable("config.php")) {
+                    trigger_error("Wrong config.php permissions. Please give config.php file 644 permission. <br> Use the Following command<br>$ chmod 644 config.php", E_USER_ERROR);
                 }
             }
 
@@ -110,19 +107,78 @@ class install {
      */
     private function setup_database() {
         global $db;
-        //Read the basic table schema and execute it
-        $scheme_file = fopen("schema.sql", "r");
-        $data = fread($scheme_file, filesize("schema.sql"));
+        //Install the tables
+
+        if (!$this->installTables()) {
+            trigger_error("Cannot create Tables", E_USER_ERROR);
+        }
+    }
+
+    private function installTables() {
+        global $db;
+        $member = $db->query("create table member (
+            id int(11) null primary key auto_increment,
+            membername mediumtext not null,
+            username mediumtext default null,
+            password mediumtext default null,
+            sonof int(11) null default null,
+            profilepic varchar(255) default null,
+            dob int(11) default null,
+            gender int(1) default 0,
+            relationship_status int(11) default 0,
+            gaon mediumtext default null,
+            related_to int(11) null default null,
+            emailid text default null,
+            alive int(1) default 0,
+            aboutme longtext default null,
+            lastlogin int(11) default null,
+            joined int(11) default null,
+            approved int(1) default 0,
+            tokenforact text default null,
+            dontshow int(1) default 0,
+            foreign key (related_to) references member(id) );");
+
+        $feedback = $db->query("create table feedback (
+            id int(11) not null primary key auto_increment,
+            user_name text not null,
+            user_emailid text not null,
+            feedback_text text not null,
+            seen int(1) default 0 );");
+
+        $joinrequest = $db->query("create table joinrequest (
+            id int(11) not null primary key auto_increment,
+            formember int(11) not null,
+            pic text default null,
+            personalmessage text default null,
+            emailid text not null,
+            tokenforact varchar(20) default null,
+            approved int(1) default 0,
+            foreign key(formember) references member(id) );");
+
+        $suggested_info = $db->query("create table suggested_info (
+            id int(11) not null primary key auto_increment,
+            typesuggest mediumtext not null,
+            suggested_value text not null,
+            suggested_by int(11) not null,
+            ts int(11) not null,
+            approved int(1) default 0,
+            foreign key(suggested_by) references member(id) );");
+
+        $suggest_approved = $db->query("create table suggest_approved (
+            id int(11) not null primary key auto_increment,
+            suggest_id int(11) not null,
+            user_id int(11) not null,
+            action int(2) not null,
+            foreign key (suggest_id) references suggested_info(id),
+            foreign key (user_id) references member(id) );");
+
+        //Now the data that we already have
+        $memberdata = file_get_contents("member_data.sql");
         
-        //$db->query($data);
+        $memberdata_sql=$db->query($memberdata);
         
-        fclose($scheme_file);
-        
-        //now enter the data that we have
-        $member_data = fopen("member_data.sql", "r");
-        $data = fread($member_data, filesize("member_data.sql"));
-        //$db->query($data);
-        fclose($member_data);
+        return $member && $feedback && $joinrequest && $suggested_info
+                && $suggest_approved && $memberdata_sql;
     }
 
 }
