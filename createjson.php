febf5993276f97c397c4a86aad4cd1b19209f7e2
piyush <piyush@piyush-Inspiron-N5110>
2013-02-10 19:04:07 +0530

    Root member also using the createstruct function now

--- createjson.php
+++ createjson.php
@@ -5,28 +5,18 @@ require "header.php";
 global $db;
 $familyid = $_GET['familyid'];
 
-//Family Id
+//Get the family id of the family to be shown
 if (!$familyid) {
     $familyid = 1;
 }
 
 
-
+//Get the member who is son of no one and is male and is of given family id
 $query = $db->query("select * from member where sonof is null and dontshow=0 and gender=0 and family_id=$familyid");
 $row = $db->fetch($query);
-$finalkey = array();
-$finalkey['id'] = $row['id'];
-$finalkey['name'] = $row['membername'];
-$finalkey['data'] = array(
-    "dob" => ($row['dob'] ? strftime($row['dob'], "%d/%m/%Y") : ""),
-    "relationship_status" => ($row['relationship_status'] == 0 ? "Single" :
-            "Married"),
-    "relationship_status_id" => $row['relationship_status'],
-    "alive" => ($row['alive'] == 0 ? "Deceased" : "Living"),
-    "gender" => $row['gender'],
-    "alive_id" => $row['alive'],
-    'image' => empty($row['profilepic']) ? "common.png" : $row['profilepic']
-);
+
+//Create a infovis struct
+$finalkey = createstruct($row);
 $finalkey['children'] = getwife($row['id']);
 
 echo json_encode($finalkey);
