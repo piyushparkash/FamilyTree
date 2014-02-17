80a973289a426fd8dc1118376a3ff31e253b11c7
Piyush <achyutapiyush@gmail.com>
2014-02-18 03:01:57 +0530

    Made file for constants

---
+++ constants.php
@@ -0,0 +1,24 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+// All the contants used in the project is used
+
+//Anything Unknown is this
+define("UNKNOWN", -255);
+
+//Relationship Related Constants
+define("SINGLE", 0);
+define("MARRIED", 1);
+
+// Gender
+define("MALE", 0);
+define("FEMALE", 1);
+
+//ALIVE
+define("ALIVE", 1);
+define("DEAD", 0);
