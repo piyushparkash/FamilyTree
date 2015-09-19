03906335157305d9f912f4fac7f962e82aa482f8
Piyush <achyutapiyush@gmail.com>
2015-09-19 13:37:20 +0530

    Improved code to use currect classes
    The file was written long back. Since then lots of functions were created
    to do things in Family Tree. Rewrote this file to use those functions. It
    is better and stable now.

--- createjson.php
+++ createjson.php
@@ -2,22 +2,20 @@
 
 require "header.php";
 
-global $db, $vanshavali;
-$familyid = $_GET['familyid'];
+global $db, $vanshavali, $user;
 
-//Get the family id of the family to be shown
-if (!$familyid) {
-    $familyid = 1;
+if ($user->is_authenticated()) {
+    $familyid = $user->user['family_id'];
+} else {
+    $familyid = null;
 }
 
-
-//Get the member who is son of no one and is male and is of given family id
-$query = $db->query("select * from member where sonof is null and dontshow=0 and gender=". MALE . " and family_id=$familyid");
-$row = $db->fetch($query);
+$head = $vanshavali->getHeadofFamily($familyid);
+$head = new member($head);
 
 //Create a infovis struct
-$finalkey = $vanshavali->createstruct($row);
-$finalkey['children'] = $vanshavali->getwife($row['id']);
+$finalkey = $vanshavali->createstruct($head->data);
+$finalkey['children'] = $vanshavali->getwife($head->data['id']);
 
 echo json_encode($finalkey);
 ?>
