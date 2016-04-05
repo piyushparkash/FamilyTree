afeffb4896ab68eac5bbdc69de812bc5c7d745c7
Piyush <achyutapiyush@gmail.com>
2016-04-05 18:24:57 +0530

    Made Few changes in admin portal.
    Aborting the work of admin portal as of now as it require compilcative path setup beforehand. As I am not able to include any file as of now.
    Will do this once I have completed codeigniter

--- admin/index.php
+++ admin/index.php
@@ -1,8 +1,32 @@
 <?php
 
 //As this is admin section. It is allowed to show errors here.
-ini_set('display_errors', 1); 
+ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 
+require_once __DIR__ . '/../config.php';
 require_once __DIR__ . '/../header.php';
+
+// first check if the user is allowed over here
+global $user, $vanshavali, $template, $db;
+
+if ($user->is_authenticated()) {
+    //Check if user is acceptable
+    $member = $vanshavali->getmember($user->get_user());
+
+    if (!$member->isAdmin()) {
+        header("Location:" . __DIR__);
+    }
+} else {
+    //This user is not supposed to be here.
+    header("Location:" . __DIR__);
+}
+
+//Now that user is authorized..Let him see the content
+
+$template->display("header.tpl");
+
+$template->display('admin/admin.main.tpl');
+
+$template->display("footer.tpl");
